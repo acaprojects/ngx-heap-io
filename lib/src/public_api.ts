d1cc1d991cdd8d901caaa95949c5ef7fe963b6b4
Alex Sorafumo <alex@yuion.net>
2019-04-16 10:53:17 +1000

    Export service in public API

--- lib/src/public_api.ts
+++ lib/src/public_api.ts
@@ -2,7 +2,4 @@
     // Export Library Module
 export * from './library.module';
     // Export Library Services
-
-    // Export Library Directives
-
-    // Export Library Classes
+export * from './services/heap-io.service';
